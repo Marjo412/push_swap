7d1b5690c77852efbdfe2f9c8a41791e8153c871
Marjo412 <marjorie_4.28@hotmail.com>
2025-04-30 12:19:05 +0200

    modif libft

---
+++ libft/ft_atol.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrosset <mrosset@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 12:12:50 by mrosset           #+#    #+#             */
+/*   Updated: 2025/04/30 12:13:41 by mrosset          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long    ft_atol(const char *str)
+{
+    int     i;
+    int     sign;
+    long    result;
+
+    i = 0;
+    sign = 1;
+    result = 0;
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i] == '-')
+            sign *= -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return (result * sign);
+}
+
+/*atoi who will return a long*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mrosset <mrosset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 11:21:24 by mrosset           #+#    #+#             */
-/*   Updated: 2025/02/09 13:20:58 by mrosset          ###   ########.fr       */
+/*   Updated: 2025/04/30 12:17:43 by mrosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@
 # include <stdlib.h>
 # include <stddef.h>
 
+
 int				ft_atoi(const char *str);
+long			ft_atol(const char *str);
 void			ft_bzero(void *src, size_t n);
 void			*ft_calloc(size_t nb, size_t len);
 int				ft_isalnum(int c);
